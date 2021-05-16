5d385d3882b303f4722204eb2ac0c4c9d432138a
Barath <barath6.99@gmail.com>
2021-05-16 19:52:34 +0530

    deploy

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const connectDB=async ()=>{
+    try {
+        const con=await mongoose.connect(process.env.MONGO_URI,{
+            useCreateIndex:true,
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
+        console.log(`mongodb connected ${con.connection.host}`)
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+    }
+}
+
+export default connectDB;
